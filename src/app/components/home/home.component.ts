0cffe09ff19f0c34be37292d2eeb02bafa6d796d
Claudio Sampaio <claudio.sampaio@pmm.am.gov.br>
2020-07-22 22:00:11 -0400

    adicionado tratamentos da reposta do catalogo dos filmes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogoService } from '../../services/catalogo.service';
+import { Filme } from 'src/app/models/filme';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  filmes: Array<number>;
+  filmes: Array<Filme>;
+  error: boolean = false;
 
-  constructor() { }
+  constructor(private catalogoService: CatalogoService) { }
 
   ngOnInit(): void {
-    this.filmes = Array(16).fill(0,0,16);
+    this.catalogoService.getCatalogo().subscribe( (catalogo) => this.filmes = catalogo,
+    (err) => {
+      this.error = true;
+      console.error(err);
+    },
+    () => console.log('FIM'));
   }
 
 }
--- src/app/services/catalogo.service.ts
+++ src/app/services/catalogo.service.ts
@@ -9,13 +9,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class CatalogoService {
 
   private hostUrl: string = 'http://copafilmes.azurewebsites.net/api/';
+  private headers: HttpHeaders = new HttpHeaders();
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { 
+    this.headers.append('content-type','application/json');
+  }
 
   /**
    * Pega a lista de filmes do servico de catalogos de filmes 
    */
   public getCatalogo(): Observable<Filme[]> {
-    return this.httpClient.get<Filme[]>(this.hostUrl + 'filmes');
+    return this.httpClient.get<Filme[]>(this.hostUrl + 'filmes', {headers: this.headers});
   }
 }
